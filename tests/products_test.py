7f839ae67d3e013ef58bd4ad575e08da58e33e5c
marianamon <Mariana.Alzate@kinesso.com>
2024-07-29 11:34:55 -0500

    comment prometheus config line

--- tests/products_test.py
+++ tests/products_test.py
@@ -1,7 +1,7 @@
 from locust.env import Environment
 from locust.stats import stats_printer, stats_history
 from locust.log import setup_logging
-from prometheus_client import start_http_server
+#from prometheus_client import start_http_server
 import gevent
 from factory.user_factory import UserFactory
 
@@ -17,7 +17,7 @@ gevent.spawn(stats_printer(env.stats))
 gevent.spawn(stats_history, env.runner)
 
 # Start the Prometheus exporter on port 8000
-start_http_server(8000)
+#start_http_server(8000)
 
 # Start the test
 env.runner.start(10, spawn_rate=2)  # 10 users, spawn rate 2 users per second
